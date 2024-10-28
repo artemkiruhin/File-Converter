58a45b4922ff0b12408f6c8e0953aabe87d65d4a
artemkiruhin <temkaunik@gmail.com>
2024-10-28 18:50:20 +0300

    Mvc controller

---
+++ FileConverter.WebApplication/Controllers/PdfController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileConverter.WebApplication.Controllers {
+    public class PdfController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
